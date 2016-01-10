7ff4543bd745c605019411dac921555d139e5c69
Manuel <manuel@iMac-de-juan.local>
2016-01-10 18:56:00 -0500

    Es el commit con ingnore

---
+++ dist/eventos.js
@@ -0,0 +1,1 @@
+function saludar(){console.log("Hola mundo")}
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp =require('gulp');
+    plumber = require('gulp-plumber'),
+    livereload=require('gulp-livereload'),
+    sass = require('gulp-sass'),
+    uglyfly = require('gulp-uglyfly'),
+    st=require('st');
+
+gulp.task('sass', function () {
+  gulp.src('scss/*.scss')
+    .pipe(plumber())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+    .pipe(livereload());
+});
+
+gulp.task('uglyfly', function() {
+  return gulp.src('javaScript/*.js')
+    .pipe(plumber())
+    .pipe(uglyfly())
+    .pipe(gulp.dest('dist'))
+    .pipe(livereload());
+});
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch('scss/*.scss', ['sass']);
+});
