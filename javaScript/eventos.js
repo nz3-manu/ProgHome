b063025f78c863a52e5562e4fa066ebd900b14e3
Manuel Ramirez <nz3.manu@gmail.com>
2016-02-08 09:36:10 -0500

    antes de usar plugins jQuery

--- javaScript/eventos.js
+++ javaScript/eventos.js
@@ -68,3 +68,24 @@ $(function(){
 
   var accordion = new Accordion($('#accordion'),false);
 });
+
+/************************************************************************
+----------------------- Menu resposible -------------------------------
+========================================================================*/
+$(document).ready(main);
+var contador = 1;
+function  main() {
+  $('.menu_bar').click(function(){
+    if (contador == 1) {
+      $('.wrap-li').animate({
+        left:'0'
+      })
+      contador = 0 ;
+    } else{
+      contador = 1;
+      $('.wrap-li').animate({
+        left:'-100%'
+      })
+    }
+  })
+};
---
+++ javaScript/eventos1.js
@@ -0,0 +1,21 @@
+$(function(){
+  $("#slider").slidesjs({
+    play: {
+      active: true,
+        // [boolean] Generate the play and stop buttons.
+        // You cannot use your own buttons. Sorry.
+      effect: "slide",
+        // [string] Can be either "slide" or "fade".
+      interval: 5000,
+        // [number] Time spent on each slide in milliseconds.
+      auto: false,
+        // [boolean] Start playing the slideshow on load.
+      swap: true,
+        // [boolean] show/hide stop and play buttons
+      pauseOnHover: false,
+        // [boolean] pause a playing slideshow on hover
+      restartDelay: 2500
+        // [number] restart delay on inactive slideshow
+    }
+  });
+});
